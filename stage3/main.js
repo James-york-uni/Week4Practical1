0f108eda2581e13627498e1406b57fdb6d731f3f
James-york-uni <zzw518@york.ac.uk>
2026-03-02 17:30:51 +0000

    3.3

---
+++ stage3/main.js
@@ -0,0 +1,19 @@
+//york clock id = york
+//id="york-am-pm"
+//class="am-pm"
+//sydney clock id = sydney
+//id="sydney-am-pm"
+//class="am-pm"
+//hand second
+//hand hour
+//hand minute
+
+function updateClock(clockID, hoursDiff) {
+    const time = new Date();
+    let hours = time.getHours();
+    console.log = time;
+    console.log = hours;
+}
+
+updateClock("york", 0);
+updateClock("sydney", 11);
